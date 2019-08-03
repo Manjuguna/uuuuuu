b4472576a35cb4fb2296320f3eda43ca4ced382a
Manjuguna <52068092+Manjuguna@users.noreply.github.com>
2019-08-03 14:41:39 +0530

    Create goat.py

---
+++ goat.py
@@ -0,0 +1,14 @@
+def sb(root): 
+    mud = len(root) 
+    sb = [1]*mud 
+    for i in range (1 , mud): 
+        for j in range(0 , i): 
+            if root[i] > root[j] and sb[i]< sb[j] + 1 : 
+                sb[i] = sb[j]+1
+    maximum = 0
+    for i in range(mud): 
+        maximum = max(maximum , sbb[i])  
+    return maximum
+ars=int(input()) 
+root = list(map(int,input().split()))
+print (sb(root))
